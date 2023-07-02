422aeb4d2a10d0d7f6032182fbef6313cfea9790
John Hiroshi <hiroshiy@scanx.com>
2023-07-02 14:50:48 +0900

    resolve conflict

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { fileURLToPath, URL } from 'node:url'
+import cesium from 'vite-plugin-cesium';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    cesium({
+      rebuildCesium: true
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
