2101c5e281540357da518747918293db56675e05
John Hiroshi <hiroshiy@scanx.com>
2023-12-16 22:59:44 +0900

    configuration

--- src/config.js
+++ src/config.js
@@ -11,18 +11,18 @@ const configuration = {
             s3Url: 'xxxx'
         },
         production: {
-            apiUrl: 'http://57.181.29.44:8080',
+            apiUrl: 'http://57.181.29.44',
             s3Url: 'https://aaa-storage-v2-prod-ap-northeast-1.s3.ap-northeast-1.amazonaws.com'
         },
-        production: {
-            broadcastUrl: 'https://echo.prod.cloud.aaa.jp',
-            apiUrl: 'https://api.prod.cloud.scanx.jp/api',
-            tileSetUrl: 'https://asset.prod.cloud.aaa.jp/static',
-            storageUrl: 'https://api.prod.cloud.aaa.jp/storage',
-            terrainUrl: 'https://asset.prod.cloud.aaa.jp/',
-            shareUrl: 'https://appv2.aaa.jp/share',
-            s3Url: 'https://aaa-storage-v2-prod-ap-northeast-1.s3.ap-northeast-1.amazonaws.com'
-        },
+        // production: {
+        //     broadcastUrl: 'https://echo.prod.cloud.aaa.jp',
+        //     apiUrl: 'https://api.prod.cloud.scanx.jp/api',
+        //     tileSetUrl: 'https://asset.prod.cloud.aaa.jp/static',
+        //     storageUrl: 'https://api.prod.cloud.aaa.jp/storage',
+        //     terrainUrl: 'https://asset.prod.cloud.aaa.jp/',
+        //     shareUrl: 'https://appv2.aaa.jp/share',
+        //     s3Url: 'https://aaa-storage-v2-prod-ap-northeast-1.s3.ap-northeast-1.amazonaws.com'
+        // },
         // vegapunk/hiroshiy@scanx.jp single sign on
         apiKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJiYzM4YTAzNS00MDI0LTRiYWQtODgxNy03ZGJlOWE2M2Q1YzciLCJpZCI6MTUxMTQ4LCJpYXQiOjE2ODg0MzM4MTR9.HwJ6PUcGbJx5lhBbgk92KTfviWtgJvY4JocmtNYediU"
     },
